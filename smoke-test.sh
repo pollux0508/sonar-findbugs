cfab971c89d549d2c35d388bf6ed026819143379
Kengo TODA <skypencil@gmail.com>
2018-11-12 11:36:25 +0800

    kick smoketest before SonarQube analysis

--- smoke-test.sh
+++ smoke-test.sh
@@ -7,7 +7,23 @@ mvn package -B
 
 docker build  -t sonar-findbugs .
 CONTAINER_ID=$(docker container run -p 9000:9000 -d sonar-findbugs)
-mvn sonar:sonar -B \
-  -Dsonar.host.url=http://localhost:9000 \
-  -Dsonar.login=admin -Dsonar.password=admin
+
+echo -n waiting SonarQube
+until $(curl --output /dev/null -s --fail http://localhost:9000); do
+    echo -n '.'
+    sleep 5
+done
+echo SonarQube has been launched.
+
+count=0
+until mvn sonar:sonar -B -Dsonar.host.url=http://localhost:9000 -Dsonar.login=admin -Dsonar.password=admin; do
+    count=$[$count+1]
+    if [ $count -ge 5 ]; then
+      echo Sonar fails to scan 5 times!
+      docker container stop $CONTAINER_ID
+      exit 1
+    fi
+    echo SonarQube is not ready to scan project, wait 5 sec
+    sleep 5
+done
 docker container stop $CONTAINER_ID
